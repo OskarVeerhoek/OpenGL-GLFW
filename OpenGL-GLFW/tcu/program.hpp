0327fac94808107674c9f69abf7299a5c54b60d2
Oskar Veerhoek <thecodinguniverse@gmail.com>
2012-07-26 14:47:50 +0200

    Added IBO.

--- OpenGL-GLFW/tcu/program.hpp
+++ OpenGL-GLFW/tcu/program.hpp
@@ -2,36 +2,35 @@
 #define OPENGL_GLFW_TCU_PROGRAM_H_
 // Include the tcu::shader contents.
 #include "shader.hpp"
-namespace tcu
+
+namespace program
 {
-	namespace program
+	// Program represents the OpenGL code that is to be executed in the application.
+	class Program
 	{
-		// Program represents the OpenGL code that is to be executed in the application.
-		class Program
-		{
-		public:
-			// Constructs an instance of Program. Does nothing else.
-			Program();
-			// Destructs the intsnace in question of Program. Calls Destroy.
-			~Program();
-			// Initializes the Program. Sets up rendering code.
-			void Init();
-			// Renders to the screen.
-			void Render();
-			// Destroys the Program. Cleans up resources.
-			void Destroy();
-		private:
-			// The OpenGL shader program.
-			tcu::shader::ShaderProgram m_shader_program; // *
-			// The OpenGL VAO (Vertex Array Object).
-			GLuint m_vao;
-			// The OpenGL VBO (Vertex Buffer Object).
-			GLuint m_vbo;
-			// An OpenGL Texture
-			GLuint m_texture;
-		};
-	}
+	public:
+		// Constructs an instance of Program. Does nothing else.
+		Program();
+		// Destructs the intsnace in question of Program. 
+		~Program();
+		// Initializes the Program. Sets up rendering code.
+		void Init();
+		// Renders to the screen.
+		void Render();
+		// Destroys the Program. Cleans up resources.
+		void Destroy();
+	private:
+		// The OpenGL shader program.
+		shader::ShaderProgram m_shader_program; // *
+		// The OpenGL VAO (Vertex Array Object)
+		GLuint m_vao;
+		// The OpenGL VBO (Vertex Buffer Object)
+		GLuint m_vbo;
+		// The OpenGL IBO (Index Buffer Object)
+		GLuint m_ibo;
+	};
 }
+
 #endif
 
 // * = I made this method/function/class myself, feel free to merge the implementation into this file.
